b4bbc44a9ced3f91f451b7936c9409521516c340
maximix <ivan82kaktus@mail.ru>
2023-03-31 21:24:17 +0400

    Update program.cpp
    Программа program.cpp рекурсивно высчитывает и выводит в стандартный вывод n-ное число фибоначчи, последовательность начинается с (0, 1, ...).

--- program.cpp
+++ program.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
+#include <iostream>
 
 int f(int n)
 {
-    if(n == 0) return 0;
-    if(n == 1 || n == 2) return 1;
+    if(n < 2) return 0;
+    if(n == 2 || n == 3) return 1;
     return f(n-1)+f(n-2);
 }
 
